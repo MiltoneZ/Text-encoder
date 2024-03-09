f5431b74587dbed21481e91475a7070bdc878925
MiltoneZ <milton.uzieldm@gmail.com>
2024-03-09 17:11:58 -0600

    Primer intento de encriptacion del texto.

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-
+let letrasParaEncriptar = [a,e,i,o,u];
+let palabraEncriptada = 0;
 
 function textoDeUsuario(encrip,texto) {
     let textUsuario = document.querySelector(texto);
     console.log(textUsuario);
+
+    return;
+}
+
+function encriptarTexto(){
+    let textoParaEncriptar = document.querySelector("#textoEncriptado");
+    console.log(textoParaEncriptar);
 
     return;
 }
