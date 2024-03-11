017d219d45071f5b9cd97612037bb7164b780da6
MiltoneZ <milton.uzieldm@gmail.com>
2024-03-10 21:00:57 -0600

    Test de algunas funciones de botones

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 let letrasParaEncriptar = [a,e,i,o,u];
 let palabraEncriptada = 0;
 
+function activadoresDeBotones(){
+    if (encrip === true) {
+        textoDeUsuario("#textoEncriptado");
+        encriptarTexto();
+        copiarTexto();
+    } else {
+        textoDeUsuario("#textoDesencriptado");
+        encriptarTexto();
+        copiarTexto();
+    }
+}
+
 function textoDeUsuario(encrip,texto) {
     let textUsuario = document.querySelector(texto);
     console.log(textUsuario);
@@ -11,6 +23,13 @@ function textoDeUsuario(encrip,texto) {
 function encriptarTexto(){
     let textoParaEncriptar = document.querySelector("#textoEncriptado");
     console.log(textoParaEncriptar);
+
+    return;
+}
+
+function copiarTexto(){
+    let textoCopiado = document.querySelector("#textoCopiado");
+    console.log(textoCopiado);
 
     return;
 }
